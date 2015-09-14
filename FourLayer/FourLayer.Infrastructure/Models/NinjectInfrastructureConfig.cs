f957a4855076056c47b1780d238a8b62d0f587fc
John Dianala <jdianala@yahoo.com>
2015-09-14 07:16:46 -0500

    try

---
+++ FourLayer/FourLayer.Infrastructure/Models/NinjectInfrastructureConfig.cs
@@ -0,0 +1,18 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourLayer.Infrastructure.Models
+{
+    public class NinjectInfrastructureConfig
+    {
+        public static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind<IRepository>().To<Repository>();
+
+        }
+    }
+}
---
+++ FourLayer/FourLayer.Services/NinjectServiceConfig.cs
@@ -0,0 +1,20 @@
+﻿using FourLayer.Infrastructure.Models;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourLayer.Services
+{
+    public class NinjectServiceConfig
+    {
+        public static void RegisterServices(IKernel kernel)
+        {
+            NinjectInfrastructureConfig.RegisterServices(kernel);
+ 
+        }
+    }
+    
+}
